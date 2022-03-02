b3ddad2e7a21a52d2f54c4cbb474c19f4273cd8a
Kevin Tindall <kevinkjt2000@gmail.com>
2022-03-02 14:22:10 -0600

    Massive speedup from primes

--- v/problem10.v
+++ v/problem10.v
@@ -1,13 +1,17 @@
 import arrays
+import math
 
 fn generate_primes() []i64 {
-	mut primes := []i64{cap: int(1e6)} // large initial capacity avoids costly reallocations
+	mut primes := []i64{cap: int(300_000)} // large initial capacity avoids costly reallocations
 	primes << 2
 	primes << 3
 
 	for i := 5; i < 2_000_000; i += 2 {
 		mut is_prime := true
 		for prime in primes {
+			if prime > math.sqrt(i) {
+				break // factor pairs past the square root are a mirrored version of previos pairs
+			}
 			if i % prime == 0 {
 				is_prime = false
 				break
